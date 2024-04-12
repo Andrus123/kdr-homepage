0806ce0258bbc6bd6d671964107474d6612432a1
Andrus123 <andruuus123@gmail.com>
2024-04-12 18:57:09 -0400

    cache next

--- next.config.js
+++ next.config.js
@@ -1,3 +1,9 @@
-module.exports = {
-    reactStrictMode: true
-  }
+const nextConfig = {
+    webpack: function (config, context) {
+    config.watchOptions = {
+        poll: 1000,
+        aggregateTimeout: 300,
+    };
+    return config;
+},
+};
